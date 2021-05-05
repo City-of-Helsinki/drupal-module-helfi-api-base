da6098c72f9d5ea6e2672bd5dbb948d2a3309018
tuutti <tuutti@iki.fi>
2021-05-05 13:04:19 +0300

    UHF-1490: Show all revisions by default

--- src/Entity/RevisionController.php
+++ src/Entity/RevisionController.php
@@ -7,7 +7,6 @@ namespace Drupal\helfi_api_base\Entity;
 use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityRepositoryInterface;
-use Drupal\Core\Language\LanguageInterface;
 use Drupal\entity\Controller\RevisionOverviewController;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -42,46 +41,40 @@ class RevisionController extends RevisionOverviewController {
    *   A render array.
    */
   protected function revisionOverview(ContentEntityInterface $entity) {
-    $langcode = $this->languageManager()
-      ->getCurrentLanguage(LanguageInterface::TYPE_CONTENT)
-      ->getId();
     /** @var \Drupal\Core\Entity\ContentEntityStorageInterface $entity_storage */
     $entity_storage = $this->entityTypeManager()->getStorage($entity->getEntityTypeId());
     $revision_ids = $this->revisionIds($entity);
     $entity_revisions = $entity_storage->loadMultipleRevisions($revision_ids);
-    $translatable = $entity->getEntityType()->isTranslatable();
 
     $header = [$this->t('Revision'), $this->t('Operations')];
     $rows = [];
     foreach ($entity_revisions as $revision) {
+      /** @var \Drupal\Core\Entity\ContentEntityInterface $revision */
       $revision = $this->entityRepository->getTranslationFromContext($revision);
 
       $row = [];
-      /** @var \Drupal\Core\Entity\ContentEntityInterface $revision */
-      if (!$translatable || ($revision->hasTranslation($langcode) && $revision->getTranslation($langcode)->isRevisionTranslationAffected())) {
-        $row[] = $this->getRevisionDescription($revision, $revision->isDefaultRevision());
+      $row[] = $this->getRevisionDescription($revision, $revision->isDefaultRevision());
 
-        if ($revision->isDefaultRevision()) {
-          $row[] = [
-            'data' => [
-              '#prefix' => '<em>',
-              '#markup' => $this->t('Current revision'),
-              '#suffix' => '</em>',
-            ],
-          ];
-          foreach ($row as &$current) {
-            $current['class'] = ['revision-current'];
-          }
+      if ($revision->isDefaultRevision()) {
+        $row[] = [
+          'data' => [
+            '#prefix' => '<em>',
+            '#markup' => $this->t('Current revision'),
+            '#suffix' => '</em>',
+          ],
+        ];
+        foreach ($row as &$current) {
+          $current['class'] = ['revision-current'];
         }
-        else {
-          $links = $this->getOperationLinks($revision);
-          $row[] = [
-            'data' => [
-              '#type' => 'operations',
-              '#links' => $links,
-            ],
-          ];
-        }
+      }
+      else {
+        $links = $this->getOperationLinks($revision);
+        $row[] = [
+          'data' => [
+            '#type' => 'operations',
+            '#links' => $links,
+          ],
+        ];
       }
 
       $rows[] = $row;
