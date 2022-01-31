142d6a0373fa0c71aeae4842e44d5162726cf3de
tuutti <tuutti@iki.fi>
2022-01-31 15:46:27 +0200

    UHF-3505: Deprecate TranslatableEntityBase, simplify migration logic

---
+++ src/Plugin/Derivative/MigrateTranslatableEntity.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\helfi_api_base\Plugin\Derivative;
+
+use Drupal\helfi_api_base\Plugin\migrate\destination\TranslatableEntity;
+use Drupal\migrate\Plugin\Derivative\MigrateEntity;
+
+/**
+ * Deriver for translatable_entity:ENTITY_TYPE entity migrations.
+ */
+class MigrateTranslatableEntity extends MigrateEntity {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDerivativeDefinitions($base_plugin_definition) : array  {
+    foreach ($this->entityDefinitions as $entity_type => $entity_info) {
+      $this->derivatives[$entity_type] = [
+        'id' => "translatable_entity:$entity_type",
+        'class' => TranslatableEntity::class,
+        'requirements_met' => 1,
+        'provider' => $entity_info->getProvider(),
+      ];
+    }
+    return $this->derivatives;
+  }
+
+}
