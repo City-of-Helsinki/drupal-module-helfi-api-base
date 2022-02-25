cda8699140e66235ac66195a6ebdf08a4dff1969
tuutti <tuutti@iki.fi>
2022-02-25 08:33:15 +0200

    UHF-4234: Fixed formatting

--- src/Plugin/Filter/LinkConverter.php
+++ src/Plugin/Filter/LinkConverter.php
@@ -25,11 +25,10 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * @Filter(
  *   id = "helfi_link_converter",
  *   title = @Translation("Hel.fi: Link converter"),
- *   description = @Translation("Runs embedded links through a template. NOTE:
- *   This filter must be run after 'Convert URLs into links' filter."), type =
- *   Drupal\filter\Plugin\FilterInterface::TYPE_MARKUP_LANGUAGE, settings = {
- *   },
- *   weight = -10
+ *   description = @Translation("Runs embedded links through a template. NOTE: This filter must be run after 'Convert URLs into links' filter."),
+ *   type = Drupal\filter\Plugin\FilterInterface::TYPE_MARKUP_LANGUAGE,
+ *   settings = {},
+ *   weight = -10 *   weight = -10
  * )
  */
 final class LinkConverter extends FilterBase implements ContainerFactoryPluginInterface {
