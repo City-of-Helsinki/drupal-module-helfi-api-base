4feb3698973d059cc8e8bd0617a82ab880f7628e
tuutti <tuutti@iki.fi>
2022-10-26 11:40:51 +0300

    UHF-7265: Custom stdout logger

---
+++ src/Logger/Stdout.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\helfi_api_base\Logger;
+
+use Drupal\Core\Logger\RfcLoggerTrait;
+use Psr\Log\LoggerInterface;
+use Drupal\Core\Logger\RfcLogLevel;
+use Drupal\Core\Logger\LogMessageParserInterface;
+
+/**
+ * This class allows logging to stdout and stderr.
+ */
+final class Stdout implements LoggerInterface {
+
+  use RfcLoggerTrait;
+
+  /**
+   * Constructs a new instance.
+   *
+   * @param \Drupal\Core\Logger\LogMessageParserInterface $parser
+   *   The parser to use when extracting message variables.
+   */
+  public function __construct(private LogMessageParserInterface $parser) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function log($level, $message, array $context = []) {
+    global $base_url;
+
+    $output = $level <= RfcLogLevel::WARNING ? 'php://stderr' : 'php://stdout';
+    $output = fopen($output, 'w');
+
+    $severity = strtoupper((string) RfcLogLevel::getLevels()[$level]);
+
+    // Populate the message placeholders and then replace them in the message.
+    $variables = $this->parser->parseMessagePlaceholders($message, $context);
+    $message = empty($variables) ? $message : strtr($message, $variables);
+
+    $fmt = '[@severity] [@type] [@date] @message | uid: @uid | request-uri: @request_uri | refer: @referer | ip:  @ip | link: @link';
+
+    $entry = strtr($fmt, [
+      '@base_url'    => $base_url,
+      '@timestamp'   => $context['timestamp'],
+      '@severity'    => $severity,
+      '@type'        => $context['channel'],
+      '@message'     => strip_tags($message),
+      '@uid'         => $context['uid'],
+      '@request_uri' => $context['request_uri'],
+      '@referer'     => $context['referer'],
+      '@ip'          => $context['ip'],
+      '@link'        => strip_tags($context['link']),
+      '@date'        => date('Y-m-d\TH:i:s', $context['timestamp']),
+    ]);
+
+    fwrite($output, $entry . "\r\n");
+    fclose($output);
+  }
+
+}
+
