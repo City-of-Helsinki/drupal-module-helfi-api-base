2f0b21afb79b776e6a21fb1d8eef18acb990495a
tuutti <tuutti@iki.fi>
2023-04-12 09:22:42 +0300

    Ignore enum file since drupal/coder does not support enums yet. See https://www.drupal.org/project/coder/issues/3283741

--- src/Environment/EnvMapping.php
+++ src/Environment/EnvMapping.php
@@ -1,9 +1,17 @@
 <?php
 
+// phpcs:ignoreFile
+// @todo remove phpcs ignore once https://www.drupal.org/project/coder/issues/3283741 is fixed.
+
 declare(strict_types = 1);
 
 namespace Drupal\helfi_api_base\Environment;
 
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+
+/**
+ * Enum to contains all available environments.
+ */
 enum EnvMapping : string {
 
   case Local = 'local';
@@ -11,5 +19,21 @@ enum EnvMapping : string {
   case Test = 'test';
   case Stage = 'stage';
   case Prod = 'prod';
+
+  /**
+   * Gets the translated label for given environment.
+   *
+   * @return \Drupal\Core\StringTranslation\TranslatableMarkup
+   *   The translated label.
+   */
+  public function label() : TranslatableMarkup {
+    return match ($this) {
+      self::Local => new TranslatableMarkup('Local'),
+      self::Dev => new TranslatableMarkup('Development'),
+      self::Test => new TranslatableMarkup('Testing'),
+      self::Stage => new TranslatableMarkup('Staging'),
+      self::Prod => new TranslatableMarkup('Production'),
+    };
+  }
 
 }
