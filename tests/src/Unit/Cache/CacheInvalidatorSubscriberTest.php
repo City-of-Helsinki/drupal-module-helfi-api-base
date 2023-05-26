3ea6c9f7257daf2e3b538f2af418c6ec0141d136
tuutti <tuutti@iki.fi>
2023-05-26 11:09:59 +0300

    UHF-8525: Added test, fixed namespace

---
+++ tests/src/Unit/Cache/CacheInvalidatorSubscriberTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\helfi_api_base\Unit\Cache;
+
+use Drupal\Core\Cache\CacheTagsInvalidatorInterface;
+use Drupal\helfi_api_base\Azure\PubSub\PubSubMessage;
+use Drupal\helfi_api_base\EventSubscriber\CacheInvalidatorSubscriber;
+use Drupal\Tests\UnitTestCase;
+use Prophecy\Argument;
+use Prophecy\PhpUnit\ProphecyTrait;
+
+/**
+ * @coversDefaultClass \Drupal\helfi_api_base\Cache\CacheTagInvalidator
+ * @group helfi_api_base
+ */
+class CacheInvalidatorSubscriberTest extends UnitTestCase {
+
+  use ProphecyTrait;
+
+  /**
+   * @covers ::__construct
+   * @covers ::invalidateTags
+   */
+  public function testInvalidCacheTags() : void {
+    $invalidator = $this->prophesize(CacheTagsInvalidatorInterface::class);
+    $invalidator->invalidateTags(Argument::any())->shouldNotBeCalled();
+    $sut = new CacheInvalidatorSubscriber($invalidator->reveal());
+    $sut->onReceive(new PubSubMessage([]));
+  }
+
+  /**
+   * @covers ::__construct
+   * @covers ::invalidateTags
+   */
+  public function testValidCacheTags() : void {
+    $invalidator = $this->prophesize(CacheTagsInvalidatorInterface::class);
+    $invalidator->invalidateTags(['node:123'])->shouldBeCalled();
+    $sut = new CacheInvalidatorSubscriber($invalidator->reveal());
+    $sut->onReceive(new PubSubMessage(['tags' => ['node:123']]));
+  }
+
+}
