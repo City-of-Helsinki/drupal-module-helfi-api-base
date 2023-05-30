a1d7faecad2fd1f2a633bae47ee740e278ca5a65
tuutti <tuutti@iki.fi>
2023-05-30 17:19:52 +0300

    Test LinkProcessor

---
+++ tests/src/Unit/Link/LinkProcessorTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\helfi_api_base\Unit\Link;
+
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\GeneratedLink;
+use Drupal\Core\Routing\UrlGeneratorInterface;
+use Drupal\Core\Url;
+use Drupal\Core\Utility\LinkGeneratorInterface;
+use Drupal\helfi_api_base\Link\InternalDomainResolver;
+use Drupal\helfi_api_base\Link\LinkProcessor;
+use Drupal\Tests\UnitTestCase;
+use Prophecy\Argument;
+
+/**
+ * @coversDefaultClass \Drupal\helfi_api_base\Link\LinkProcessor
+ * @group helfi_api_base
+ */
+class LinkProcessorTest extends UnitTestCase {
+
+  /**
+   * @covers ::preRenderLink
+   */
+  public function testPreRenderLink() : void {
+    $container = new ContainerBuilder();
+    $container->set('helfi_api_base.internal_domain_resolver', new InternalDomainResolver(['www.hel.fi']));
+    $container->set('url_generator', $this->prophesize(UrlGeneratorInterface::class)->reveal());
+    $linkGenerator = $this->prophesize(LinkGeneratorInterface::class);
+    $linkGenerator->generate(Argument::any(), Argument::any())
+      ->willReturn(new GeneratedLink());
+    $container->set('link_generator', $linkGenerator->reveal());
+    \Drupal::setContainer($container);
+
+    $url = Url::fromUri('tel:123456', ['absolute' => TRUE]);
+
+    $element = [
+      '#url' => $url,
+      '#title' => 'something',
+    ];
+    $render = LinkProcessor::preRenderLink($element);
+    $this->assertEquals([
+      'data-is-external' => 'true',
+      'data-protocol' => 'tel',
+    ], $render['#attributes']);
+  }
+
+}
