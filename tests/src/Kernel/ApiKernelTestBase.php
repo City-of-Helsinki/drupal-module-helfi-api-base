9c2dedcbb7a2b5f3b6f5c38282670b3d97905a73
tuutti <tuutti@iki.fi>
2023-06-02 07:23:47 +0300

    Moved ::processRequest and ::getMockedRequest to ApiTestTrait

--- tests/src/Kernel/ApiKernelTestBase.php
+++ tests/src/Kernel/ApiKernelTestBase.php
@@ -4,13 +4,10 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\helfi_api_base\Kernel;
 
-use Drupal\Component\Serialization\Json;
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\DependencyInjection\ServiceModifierInterface;
 use Drupal\KernelTests\Core\Entity\EntityKernelTestBase;
 use Drupal\Tests\helfi_api_base\Traits\ApiTestTrait;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 /**
  * API test base.
@@ -39,50 +36,6 @@ abstract class ApiKernelTestBase extends EntityKernelTestBase implements Service
    * {@inheritdoc}
    */
   public function alter(ContainerBuilder $container) {
-  }
-
-  /**
-   * Process a request.
-   *
-   * @param \Symfony\Component\HttpFoundation\Request $request
-   *   The request.
-   *
-   * @return \Symfony\Component\HttpFoundation\Response
-   *   The response.
-   */
-  protected function processRequest(Request $request): Response {
-    return $this->container->get('http_kernel')->handle($request);
-  }
-
-  /**
-   * Creates a request object.
-   *
-   * @param string $uri
-   *   The uri.
-   * @param string $method
-   *   The method.
-   * @param array $parameters
-   *   The parameters.
-   * @param array $document
-   *   The document.
-   *
-   * @return \Symfony\Component\HttpFoundation\Request
-   *   The request.
-   */
-  protected function getMockedRequest(
-    string $uri,
-    string $method = 'GET',
-    array $parameters = [],
-    array $document = []
-  ): Request {
-    $document = $document ? Json::encode($document) : NULL;
-
-    $request = Request::create($uri, $method, $parameters, [], [], [], $document);
-    if ($document !== []) {
-      $request->headers->set('Content-Type', 'application/json');
-    }
-    $request->headers->set('Accept', 'application/json');
-    return $request;
   }
 
 }
