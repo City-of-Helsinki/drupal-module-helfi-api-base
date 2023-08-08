b1eee9277fb5924b4d4137148690989bd591f5d3
Dragos Dumitrescu <dragos.dumitrescu@ibm.com>
2023-08-08 11:33:56 +0300

    PLATTA-4940 - fixes after review

--- src/Link/LinkProcessor.php
+++ src/Link/LinkProcessor.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace Drupal\helfi_api_base\Link;
 
 use Drupal\Core\Render\Element\Link;
+use Drupal\Core\Render\Markup;
 use Drupal\Core\Url;
 
 /**
@@ -16,49 +17,39 @@ final class LinkProcessor extends Link {
    * {@inheritdoc}
    */
   public static function preRenderLink($element) : array {
+    $useTemplate = FALSE;
+
     if (isset($element['#url']) && $element['#url'] instanceof Url) {
+      $url = $element['#url'];
+
+      if ($element['#title'] instanceof Markup) {
+        $useTemplate = TRUE;
+      }
+
       /** @var \Drupal\helfi_api_base\Link\InternalDomainResolver $resolver */
       $resolver = \Drupal::service('helfi_api_base.internal_domain_resolver');
 
-      // In some cases we need to enrich title, so we use #theme helfi_link
-      // but in cases where this is not needed, having a twig for any title
-      // adds unnecessary load time, so we need to be specific when to use helfi_link.
-      $use_helfi_link = FALSE;
-
       // We can't set URI's 'external' property to FALSE, because it will
       // break the URL validation.
-      if ($resolver->isExternal($element['#url'])) {
+      if ($resolver->isExternal($url)) {
         $element['#attributes']['data-is-external'] = 'true';
 
-        if ($scheme = $resolver->getProtocol($element['#url'])) {
+        if ($scheme = $resolver->getProtocol($url)) {
           $element['#attributes']['data-protocol'] = $scheme;
         }
-      }
-
-      if (!empty($element['#attributes']['data-is-external'])) {
-        $use_helfi_link = TRUE;
-      }
-      if (!empty($element['#attributes']['data-protocol'])) {
-        $use_helfi_link = TRUE;
-      }
-
-      /** @var \Drupal\Core\Url $url */
-      $url = $element['#url'];
-      $url_attributes = $url->getOption('attributes');
-      if (!empty($url_attributes['data-selected-icon'])
-        || !empty($element['#attributes']['data-selected-icon'])) {
-        $use_helfi_link = TRUE;
-      }
-
-      if ($use_helfi_link) {
-        $element['#title'] = [
-          '#theme' => 'helfi_link',
-          '#url' => $element['#url'],
-          '#title' => $element['#title'],
-        ];
-        $element['#title']['#attributes'] = $element['#attributes'] ?? [];
+        $useTemplate = TRUE;
       }
     }
+
+    if ($useTemplate) {
+      $element['#title'] = [
+        '#theme' => 'helfi_link',
+        '#url' => $element['#url'],
+        '#title' => $element['#title'],
+      ];
+      $element['#title']['#attributes'] = $element['#attributes'] ?? [];
+    }
+
     return parent::preRenderLink($element);
   }
 
