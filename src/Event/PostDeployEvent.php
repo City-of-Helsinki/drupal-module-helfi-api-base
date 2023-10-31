34e910c796ed1297215d79647ec7eab50136bf3e
tuutti <tuutti@iki.fi>
2023-10-31 14:19:30 +0200

    Fixed base class

--- src/Event/PostDeployEvent.php
+++ src/Event/PostDeployEvent.php
@@ -4,7 +4,7 @@ declare(strict_types = 1);
 
 namespace Drupal\helfi_api_base\Event;
 
-use Drupal\Component\EventDispatcher\Event;
+use Symfony\Contracts\EventDispatcher\Event;
 
 /**
  * Post-deploy event.
