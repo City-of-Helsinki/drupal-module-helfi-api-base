86a9fa468fd69d623d0f387fef04b621b83daac6
tuutti <tuutti@iki.fi>
2024-04-17 16:46:08 +0300

    UHF-9978: Interface for CacheTagInvalidator class

---
+++ src/Cache/CacheTagInvalidatorInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\helfi_api_base\Cache;
+
+/**
+ * A service to invalidate cache tags on all instances.
+ */
+interface CacheTagInvalidatorInterface {
+
+  /**
+   * Invalidates given cache tags.
+   *
+   * @param array $tags
+   *   An array of cache tags.
+   * @param array $instances
+   *   The instances to flush caches from.
+   */
+  public function invalidateTags(
+    array $tags,
+    array $instances = []
+  ): void;
+
+}
