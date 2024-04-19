27bc1583829fb5848b866f058c7be5f8bac732e9
tuutti <tuutti@iki.fi>
2024-04-19 11:52:37 +0300

    UHF-9635: Make sure event subscriber is actually called

---
+++ tests/src/Kernel/EventSubscriber/SentryTracesSamplerSubscriberTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\helfi_api_base\Kernel\EventSubscriber;
+
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\Tests\helfi_api_base\Traits\ApiTestTrait;
+use Sentry\SentrySdk;
+
+/**
+ * Tests Rotate uid1 password.
+ *
+ * @group helfi_api_base
+ */
+class SentryTracesSamplerSubscriberTest extends KernelTestBase {
+
+  use ApiTestTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'user',
+    'raven',
+    'helfi_api_base',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    $this->installConfig('helfi_api_base');
+  }
+
+  /**
+   * Make sure our 'traces_sampler' is actually registered.
+   */
+  public function testSamplerIsCalled() : void {
+    $request = $this->getMockedRequest('/user/login');
+    $this->processRequest($request);
+
+    $client = SentrySdk::getCurrentHub()->getClient();
+    $sampler = $client->getOptions()->getTracesSampler();
+    $this->assertIsCallable($sampler);
+  }
+
+}
