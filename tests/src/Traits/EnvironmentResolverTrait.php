122bfe37a0091982218ad65671ac1449ce77fe32
tuutti <tuutti@iki.fi>
2026-04-02 11:26:59 +0300

    UHF-13066: phpcs fixes

--- tests/src/Traits/EnvironmentResolverTrait.php
+++ tests/src/Traits/EnvironmentResolverTrait.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\helfi_api_base\Traits;
 
-use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Tests\UnitTestCase;
 use Drupal\helfi_api_base\Environment\EnvironmentEnum;
 use Drupal\helfi_api_base\Environment\EnvironmentResolver;
